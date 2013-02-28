46107f346ffb2ad5fe7320c76c32ea1af8cad585
veosotano <veosotano@gmail.com>
2013-02-28 23:59:10 +0000

    Select the inner element by class name, instead of by index, so that it continues working correctly if additional elements have been injected before it

--- jquery.customSelect.js
+++ jquery.customSelect.js
@@ -22,7 +22,7 @@
                     var $select = $(el),
                         currentSelected = $select.find(':selected'),
                         customSelectSpan = $select.next(),
-                        customSelectSpanInner = customSelectSpan.children(':first'),
+                        customSelectSpanInner = customSelectSpan.children('.customSelectInner'),
                         html = currentSelected.html() || '&nbsp;';
 
                     customSelectSpanInner.html(html);
